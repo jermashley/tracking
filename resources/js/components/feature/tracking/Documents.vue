781572f1994699909d04838597051aad5217aefe
Mohamed EL AZHARI <melazhari@prologuetechnology.com>
2025-06-17 17:06:03 -0500

    feature/SD2-43468 adding shipment document feature

--- resources/js/components/feature/tracking/Documents.vue
+++ resources/js/components/feature/tracking/Documents.vue
@@ -1,26 +1,31 @@
 <script setup>
+import {ref} from "vue";
+
 const props = defineProps({
   documents: {
     type: Array,
     required: true,
   },
 })
-</script>
 
+const notEmptyDocumentsList = ref(true)
+</script>
 <template>
-  <h2 class="text-lg font-semibold mb-4">Shipment Documents</h2>
-  <table>
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>URL</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="document in documents" :key="document.url">
-        <td>{{ document.name }}</td>
-        <td>&nbsp; &nbsp; <a :href="document.url" target="_blank">Download</a></td>
-      </tr>
-    </tbody>
-  </table>
+  <template v-if="notEmptyDocumentsList">
+    <h2 class="text-lg font-semibold mb-4">Shipment Documents</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="document in documents" :key="document.url">
+          <td>{{ document.name }}</td>
+          <td>&nbsp; &nbsp; <a :href="document.url" target="_blank">Download</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
 </template>
