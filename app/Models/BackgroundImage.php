8b45b24964c400de57811330febc7bee3088aa9d
Jeremiah Ashley <jeremiah.ashley@live.com>
2025-06-18 21:21:56 -0500

    🛟 ide helper stuff

--- app/Models/BackgroundImage.php
+++ app/Models/BackgroundImage.php
@@ -7,6 +7,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $name
+ * @property string $url
+ * @property int $is_system
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Company> $companies
+ * @property-read int|null $companies_count
+ * @method static \Database\Factories\BackgroundImageFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereIsSystem($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BackgroundImage whereUuid($value)
+ * @mixin \Eloquent
+ */
 class BackgroundImage extends Model
 {
     /** @use HasFactory<\Database\Factories\BackgroundFactory> */
